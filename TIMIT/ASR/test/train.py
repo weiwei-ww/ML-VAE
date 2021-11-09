ad16f43413823e417d026b5c8b9026c6d129b0a0
Wei Wei <weiwei-ww@outlook.com>
2021-11-09 17:13:30 +0800

    Minor fix

--- TIMIT/ASR/test/train.py
+++ TIMIT/ASR/test/train.py
@@ -14,16 +14,16 @@ class ASR_Brain(sb.Brain):
         self.label_encoder = label_encoder
 
     def compute_forward(self, batch, stage):
-        "Given an input batch it computes the phoneme probabilities."
+        'Given an input batch it computes the phoneme probabilities.'
         batch = batch.to(self.device)
         wavs, wav_lens = batch.sig
         # Adding optional augmentation when specified:
         if stage == sb.Stage.TRAIN:
-            if hasattr(self.hparams, "env_corrupt"):
+            if hasattr(self.hparams, 'env_corrupt'):
                 wavs_noise = self.hparams.env_corrupt(wavs, wav_lens)
                 wavs = torch.cat([wavs, wavs_noise], dim=0)
                 wav_lens = torch.cat([wav_lens, wav_lens])
-            if hasattr(self.hparams, "augmentation"):
+            if hasattr(self.hparams, 'augmentation'):
                 wavs = self.hparams.augmentation(wavs, wav_lens)
 
         feats = self.hparams.compute_features(wavs)
@@ -35,7 +35,7 @@ class ASR_Brain(sb.Brain):
         return pout, wav_lens
 
     def compute_objectives(self, predictions, batch, stage):
-        "Given the network predictions and targets computed the CTC loss."
+        'Given the network predictions and targets computed the CTC loss.'
         pout, pout_lens = predictions
         phns, phn_lens = batch.phn_encoded
 
@@ -62,42 +62,42 @@ class ASR_Brain(sb.Brain):
         return loss
 
     def on_stage_start(self, stage, epoch):
-        "Gets called when a stage (either training, validation, test) starts."
+        'Gets called when a stage (either training, validation, test) starts.'
         self.ctc_metrics = self.hparams.ctc_stats()
 
         if stage != sb.Stage.TRAIN:
             self.per_metrics = self.hparams.per_stats()
 
     def on_stage_end(self, stage, stage_loss, epoch):
-        """Gets called at the end of a stage."""
+        '''Gets called at the end of a stage.'''
         if stage == sb.Stage.TRAIN:
             self.train_loss = stage_loss
         else:
-            per = self.per_metrics.summarize("error_rate")
+            per = self.per_metrics.summarize('error_rate')
 
         if stage == sb.Stage.VALID:
             old_lr, new_lr = self.hparams.lr_annealing(per)
             sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
             self.hparams.train_logger.log_stats(
-                stats_meta={"epoch": epoch, "lr": old_lr},
-                train_stats={"loss": self.train_loss},
-                valid_stats={"loss": stage_loss, "PER": per},
+                stats_meta={'epoch': epoch, 'lr': old_lr},
+                train_stats={'loss': self.train_loss},
+                valid_stats={'loss': stage_loss, 'PER': per},
             )
             self.checkpointer.save_and_keep_only(
-                meta={"PER": per}, min_keys=["PER"],
+                meta={'PER': per}, min_keys=['PER'],
             )
 
         elif stage == sb.Stage.TEST:
             self.hparams.train_logger.log_stats(
-                stats_meta={"Epoch loaded": self.hparams.epoch_counter.current},
-                test_stats={"loss": stage_loss, "PER": per},
+                stats_meta={'Epoch loaded': self.hparams.epoch_counter.current},
+                test_stats={'loss': stage_loss, 'PER': per},
             )
-            with open(self.hparams.wer_file, "w") as w:
-                w.write("CTC loss stats:\n")
+            with open(self.hparams.wer_file, 'w') as w:
+                w.write('CTC loss stats:\n')
                 self.ctc_metrics.write_stats(w)
-                w.write("\nPER stats:\n")
+                w.write('\nPER stats:\n')
                 self.per_metrics.write_stats(w)
-                print("CTC and PER stats written to ", self.hparams.wer_file)
+                print('CTC and PER stats written to ', self.hparams.wer_file)
 
 
 def data_io_prep(hparams):
@@ -134,12 +134,11 @@ def data_io_prep(hparams):
 
     # 5. Fit encoder:
     with open('dict.txt') as f:
-        phoneme_dict = [line.split()[0] for line in f.readlines() if not line.startswith('err')][3:]
-        # phoneme_dict = [line.split()[0] for line in f.readlines()][3:]
+        # phoneme_dict = [line.split()[0] for line in f.readlines() if not line.startswith('err')][3:]
+        phoneme_dict = [line.split()[0] for line in f.readlines()][3:]
     label_encoder.update_from_iterable(phoneme_dict, sequence_input=False)
     label_encoder.insert_blank(index=hparams['blank_index'])
     label_encoder.add_unk()
-    label_encoder.add_label('err')
     lab_enc_file = os.path.join(hparams['save_folder'], 'label_encoder.txt')
     label_encoder.save(lab_enc_file)
     # label_encoder.load_or_create(
@@ -180,11 +179,11 @@ if __name__ == '__main__':
     # initialize model
     asr_brain = ASR_Brain(
         label_encoder=label_encoder,
-        modules=hparams["modules"],
-        opt_class=hparams["opt_class"],
+        modules=hparams['modules'],
+        opt_class=hparams['opt_class'],
         hparams=hparams,
         run_opts=run_opts,
-        checkpointer=hparams["checkpointer"],
+        checkpointer=hparams['checkpointer'],
     )
 
     # fit the model
@@ -193,7 +192,13 @@ if __name__ == '__main__':
         asr_brain.hparams.epoch_counter,
         train_dataset,
         valid_dataset,
-        train_loader_kwargs=hparams["train_dataloader_opts"],
-        valid_loader_kwargs=hparams["valid_dataloader_opts"],
+        train_loader_kwargs=hparams['train_dataloader_opts'],
+        valid_loader_kwargs=hparams['valid_dataloader_opts'],
     )
 
+    # Test
+    asr_brain.evaluate(
+        test_dataset,
+        min_key='PER',
+        test_loader_kwargs=hparams['test_dataloader_opts'],
+    )
