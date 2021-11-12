f5f3c343a521894a95100b5cb3333acbf4ef1db0
Wei Wei <weiwei-ww@outlook.com>
2021-11-13 03:24:07 +0800

    Add codes for computing F1

--- src/utils/metric_stats.py
+++ src/utils/metric_stats.py
@@ -14,7 +14,7 @@ class MDMetricStats:
     def append(self, ids, **kwargs):
         self.ids.extend(ids)
         self.scores_list.extend(utils.md_scoring.batch_seq_md_scoring(**kwargs))
-        if len(self.md_metric_keys):
+        if len(self.md_metric_keys) == 0:
             self.md_metric_keys = list(self.scores_list[0].keys())
 
     def summarize(self):
@@ -25,3 +25,10 @@ class MDMetricStats:
         for key in self.md_metric_keys:
             mean_scores[key] = [scores[key] for scores in self.scores_list]
             mean_scores[key] = torch.mean(torch.tensor(mean_scores[key]))
+
+        eps = 1e-6
+        PRE = mean_scores['PRE']
+        REC = mean_scores['REC']
+        mean_scores['F1'] = (2 * PRE * REC) / (PRE + REC + eps)
+
+        return mean_scores
