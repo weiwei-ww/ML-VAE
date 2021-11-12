76f8afa8946a2120a140e698924080c01032e075
Wei Wei <weiwei-ww@outlook.com>
2021-11-13 01:58:59 +0800

    Update codes for pipelines

--- src/utils/data_io.py
+++ src/utils/data_io.py
@@ -1,3 +1,5 @@
+import librosa
+
 import torch
 import speechbrain as sb
 import speechbrain.dataio.dataio
@@ -5,11 +7,11 @@ import speechbrain.dataio.dataset
 import speechbrain.dataio.encoder
 import speechbrain.utils.data_pipeline
 
+from utils.preprocessing import generate_flvl_annotation
+
 
 def data_io_prep(hparams):
-    'Creates the datasets and their data processing pipelines.'
-
-    # 1. Define datasets:
+    # datasets definition
     def dataset_prep(hparams, set_name):
         dataset = sb.dataio.dataset.DynamicItemDataset.from_json(
             json_path=hparams['prepare'][f'{set_name}_json_path'])
@@ -28,52 +30,94 @@ def data_io_prep(hparams):
 
     label_encoder = sb.dataio.encoder.CTCTextEncoder()
 
-    # 2. Define audio pipeline:
-    # sb.dataio.dataset.add_dynamic_item(datasets, sb.dataio.dataio.read_audio, takes='wav_path', provides='sig')
-
+    # audio pipelines
     @speechbrain.utils.data_pipeline.takes('wav_path')
-    @speechbrain.utils.data_pipeline.provides('wav', 'feat', 'augmented_wav', 'augmented_feat')
+    @speechbrain.utils.data_pipeline.provides('wav', 'feat', 'aug_wav', 'aug_feat')
     def audio_pipeline(wav_path):
-        wav = speechbrain.dataio.dataio.read_audio(wav_path)
-        yield wav
+        # use librosa instead of sb to get the correct sample rate
+        sr = hparams['sample_rate']
+        wav, _ = librosa.load(wav_path, sr=sr)
+        wav = torch.from_numpy(wav)
+        # wav = speechbrain.dataio.dataio.read_audio(wav_path)
+        yield wav  # wave form
 
         batched_wav = wav.unsqueeze(dim=0)  # add a batch dimension
         feat = hparams['compute_features'](batched_wav).squeeze(dim=0)
-        yield feat
+        # print(feat.shape)
+        yield feat  # feature
 
-        augmented_wav = wav
+        aug_wav = wav
         if hparams.get('augmentation'):
-            augmented_wav = hparams['augmentation'](batched_wav, torch.ones(1)).squeeze(dim=0)
-        yield augmented_wav
+            aug_wav = hparams['augmentation'](batched_wav, torch.ones(1)).squeeze(dim=0)
+        yield aug_wav  # wave form with augmentation
 
-        batched_augmented_wav = augmented_wav.unsqueeze(dim=0)
-        augmented_feat = hparams['compute_features'](batched_augmented_wav).squeeze(dim=0)
-        yield augmented_feat
+        batched_aug_wav = aug_wav.unsqueeze(dim=0)
+        aug_feat = hparams['compute_features'](batched_aug_wav).squeeze(dim=0)
+        yield aug_feat  # feature with augmentation
 
     sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
 
+    # text pipelines
+    # frame level phonemes
+    @speechbrain.utils.data_pipeline.takes('feat', 'aug_feat', 'duration', 'gt_segmentation', 'phoneme_seq')
+    @speechbrain.utils.data_pipeline.provides('gt_phn', 'flvl_gt_phn', 'aug_flvl_gt_phn')
+    def flvl_phoneme_pipeline(feat, aug_feat, duration, segmentation, phoneme_seq):
+        gt_phn = label_encoder.encode_sequence_torch(phoneme_seq)
+        yield gt_phn  # encoded phonemes
+        flvl_gt_phn = generate_flvl_annotation(label_encoder, feat, duration, segmentation, gt_phn)
+        yield flvl_gt_phn  # frame level phonemes
+        aug_flvl_gt_phn = generate_flvl_annotation(label_encoder, aug_feat, duration, segmentation, gt_phn)
+        yield aug_flvl_gt_phn  # frame level phonemes with augmentation
+    sb.dataio.dataset.add_dynamic_item(datasets, flvl_phoneme_pipeline)
 
+    # frame level canonicals
+    @speechbrain.utils.data_pipeline.takes('feat', 'aug_feat', 'duration', 'gt_segmentation', 'gt_cnncl')
+    @speechbrain.utils.data_pipeline.provides('gt_cnncl', 'flvl_gt_cnncl', 'aug_flvl_gt_cnncl')
+    def flvl_canonical_pipeline(feat, aug_feat, duration, segmentation, gt_cnncl):
+        gt_cnncl = label_encoder.encode_sequence_torch(gt_cnncl)
+        yield gt_cnncl  # encoded canonicals
+        flvl_gt_cnncl = generate_flvl_annotation(label_encoder, feat, duration, segmentation, gt_cnncl)
+        yield flvl_gt_cnncl  # frame level canonicals
+        aug_flvl_gt_cnncl = generate_flvl_annotation(label_encoder, aug_feat, duration, segmentation, gt_cnncl)
+        yield aug_flvl_gt_cnncl  # frame level canonicals with augmentation
+    sb.dataio.dataset.add_dynamic_item(datasets, flvl_canonical_pipeline)
 
-    # 3. Define text pipeline:
-    sb.dataio.dataset.add_dynamic_item(datasets, lambda p: label_encoder.encode_sequence_torch(p),
-                                       takes='phoneme_list', provides='encoded_phoneme_list')
-    sb.dataio.dataset.add_dynamic_item(datasets, lambda p: label_encoder.encode_sequence_torch(p),
-                                       takes='canonical_list', provides='encoded_canonical_list')
+    # MD ground truth pipelines
+    # phoneme level ground truth
+    @speechbrain.utils.data_pipeline.takes('gt_phn', 'gt_cnncl')
+    @speechbrain.utils.data_pipeline.provides('plvl_gt_md_lbl')
+    def plvl_gt_md_lbl_pipeline(gt_phn, gt_cnncl):
+        return torch.ne(gt_phn, gt_cnncl).long()
+    sb.dataio.dataset.add_dynamic_item(datasets, plvl_gt_md_lbl_pipeline)
 
-    # 4. Set output:
+    # frame level ground truth
+    @speechbrain.utils.data_pipeline.takes('flvl_gt_phn', 'flvl_gt_cnncl', 'aug_flvl_gt_phn', 'aug_flvl_gt_cnncl')
+    @speechbrain.utils.data_pipeline.provides('flvl_gt_md_lbl', 'aug_flvl_gt_md_lbl')
+    def flvl_gt_md_lbl_pipeline(flvl_gt_phn, flvl_gt_cnncl, aug_flvl_gt_phn, aug_flvl_gt_cnncl):
+        flvl_gt_md_lbl = torch.ne(flvl_gt_phn, flvl_gt_cnncl).long()
+        yield flvl_gt_md_lbl
+        aug_flvl_gt_md_lbl = torch.ne(aug_flvl_gt_phn, aug_flvl_gt_cnncl).long()
+        yield aug_flvl_gt_md_lbl
+    sb.dataio.dataset.add_dynamic_item(datasets, flvl_gt_md_lbl_pipeline)
+
+    # set output keys
     output_keys = [
         'id',
-        'encoded_phoneme_list', 'encoded_canonical_list',
-        'wav', 'augmented_wav',
-        'feat', 'augmented_feat',
-        'gt_segmentation'
+        'wav', 'aug_wav',  # wave form
+        'feat', 'aug_feat',  # feature
+        'gt_phn', 'gt_cnncl',  # encoded phonemes
+        'flvl_gt_phn', 'flvl_gt_cnncl',  # frame level phonemes
+        'aug_flvl_gt_cnncl', 'aug_flvl_gt_cnncl',  # frame level phoneme with augmentation
+        'plvl_gt_md_lbl', 'flvl_gt_md_lbl', 'aug_flvl_gt_md_lbl',  # phoneme and frame level MD ground truth
+        'gt_segmentation'  # ground truth segmentation
     ]
     sb.dataio.dataset.set_output_keys(datasets, output_keys)
 
-    # 5. Fit encoder:
+    # fit the label encoder
     phoneme_set = hparams['prepare']['phoneme_set_handler'].get_phoneme_set()
-
     label_encoder.update_from_iterable(phoneme_set, sequence_input=False)
     label_encoder.insert_blank(index=hparams['blank_index'])
+
+    print(datasets[0][0])
 
     return train_dataset, valid_dataset, test_dataset, label_encoder
