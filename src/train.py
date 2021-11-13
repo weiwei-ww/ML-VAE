c552410c66672ac7023182affa67467bee4db700
Wei Wei <weiwei-ww@outlook.com>
2021-11-14 03:01:19 +0800

    Add codes for extra overrides

--- src/train.py
+++ src/train.py
@@ -1,7 +1,9 @@
 import sys
 import importlib
+import ruamel.yaml
 
 from hyperpyyaml import load_hyperpyyaml
+from hyperpyyaml.core import recursive_update
 import speechbrain as sb
 
 from utils.data_io import data_io_prep
@@ -12,9 +14,14 @@ if __name__ == '__main__':
     # parse command line args
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
 
+    # load extra overrides
+    overrides = ruamel.yaml.YAML().load(overrides)
+    extra_overrides = overrides.pop('extra_overrides', {})
+
     # load hparams
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
+    recursive_update(hparams, extra_overrides)
 
 
     # json file preparation
