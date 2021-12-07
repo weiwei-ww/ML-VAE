4f9506cde0233d48e15250cd2e4dcf3c2d6e452e
Wei Wei <weiwei-ww@outlook.com>
2021-12-08 04:57:45 +0800

    Add codes for w2v_CRDNN_CTC_cnncl

---
+++ src/models/w2v_CRDNN_CTC_cnncl/model.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+import speechbrain as sb
+from speechbrain.utils.data_utils import undo_padding
+from speechbrain.utils.metric_stats import ErrorRateStats
+from speechbrain.nnet.losses import ctc_loss
+
+import utils.alignment
+from utils.metric_stats.md_metric_stats import MDMetricStats
+import models.CRDNN_CTC_cnncl.model as CRDNN_CTC_cnncl
+
+logger = logging.getLogger(__name__)
+
+
+class SBModel(CRDNN_CTC_cnncl.SBModel):
+
+    def compute_forward(self, batch, stage):
+        batch = batch.to(self.device)
+
+        wavs, wav_lens = batch['wav']
+        feats = self.modules['wav2vec2'](wavs)  # (B, T, 1024)
+
+        out = self.modules.crdnn(feats)
+        out = self.modules.output(out)
+        pout = self.hparams.log_softmax(out)
+
+        predictions = {
+            'pout': pout,
+        }
+
+        return predictions
