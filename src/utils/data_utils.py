dc3b5e526ecc9d0c54f40bdcdfb6fd85d193414d
Wei Wei <weiwei-ww@outlook.com>
2021-12-09 04:59:55 +0800

    Update codes for applying weight

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,5 +1,7 @@
 import torch
 
+from speechbrain.nnet.losses import length_to_mask
+
 
 def undo_padding_tensor(batch, lengths):
     """
@@ -48,7 +50,55 @@ def apply_weight(x, weight):
     if x.ndim == 3:
         C = C // N
         x = x.view(B, T, N, C)
-    weight = weight.view(B, T, 1, N)
+
+    # reshape tensors
+    x = x.view(B * T, N, C)
+    weight = weight.view(B * T, 1, N)
+
+    # apply weight
     weighted_x = torch.bmm(weight, x)
+
+    # reshape weighted x
     weighted_x = weighted_x.view(B, T, C)
+
     return weighted_x
+
+
+def apply_lens_to_loss(loss, lens, reduction='mean'):
+    """
+    Compute the mean loss of a batch while considering the lengths of each sample.
+
+    Parameters
+    ----------
+    loss : torch.tensor
+        (B, T, C)
+    lens : torch.Tensor
+        (B)
+    reduction : str
+        'mean', 'batchmean' or 'batch'
+
+    Returns
+    -------
+    loss : torch.tensor
+        Single value. Mean loss.
+    """
+    # compute and apply mask
+    mask = torch.ones_like(loss)
+    length_mask = length_to_mask(lens * mask.shape[1], max_len=mask.shape[1])
+    length_mask = length_mask.type(mask.dtype)
+    # handle any dimensionality of input
+    while len(length_mask.shape) < len(mask.shape):
+        length_mask = length_mask.unsqueeze(-1)
+    mask *= length_mask
+
+    # compute loss
+    loss = loss * mask
+    B = loss.size(0)
+    if reduction == 'mean':
+        loss = loss.sum() / torch.sum(mask)
+    elif reduction == 'batchmean':
+        loss = loss.sum() / B
+    elif reduction == 'batch':
+        loss = loss.reshape(B, -1).sum(dim=-1) / mask.reshape(B, -1).sum(dim=-1)
+
+    return loss
