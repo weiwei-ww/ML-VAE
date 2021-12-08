999a704e5732ff7eed1815a2b97a34d4813e5d2e
Wei Wei <weiwei-ww@outlook.com>
2021-12-09 05:00:05 +0800

    Add codes for h_vae

---
+++ src/models/test_h_vae/model.py
@@ -0,0 +1,72 @@
+import logging
+
+from torch.distributions import Categorical
+import torch.nn
+from speechbrain.nnet.losses import compute_masked_loss
+
+from models.md_model import MDModel
+from utils.data_utils import apply_lens_to_loss
+
+logger = logging.getLogger(__name__)
+
+
+class SBModel(MDModel):
+    def compute_forward(self, batch, stage):
+        batch = batch.to(self.device)
+        feats, feat_lens = batch['feat']
+
+        # feature normalization
+        current_epoch = self.hparams.epoch_counter.current
+        feats = self.hparams.normalizer(feats, feat_lens, epoch=current_epoch)
+
+        # RNN
+        rnn_out = self.modules['rnn'](feats)[0]  # (B, T, C)
+
+        # compute Pi
+        pi_logits = self.modules['pi_fc'](rnn_out)  # (B, T, 2)
+        dist = Categorical(logits=pi_logits)
+        if self.modules.training:
+            sampled_pi = dist.sample().float()  # (B, T)
+        else:
+            sampled_pi = torch.argmax(pi_logits, dim=-1).float()  # (B, T)
+        sampled_pi = torch.stack([1 - sampled_pi, sampled_pi], dim=2)  # (B, T, 2)
+
+        # VAE encoder and decoder
+        encoder_out = self.modules['encoder'](rnn_out, sampled_pi)
+        sampled_h = encoder_out['sampled_h']  # (B, T, C)
+        decoder_out = self.modules['decoder'](sampled_h)
+
+        vae_losses = encoder_out['losses']
+
+        predictions = {
+            'encoder_out': encoder_out,
+            'decoder_out': decoder_out,
+            'losses': vae_losses
+        }
+
+        return predictions
+
+    def compute_objectives(self, predictions, batch, stage):
+        # get model outputs
+        encoder_out = predictions['encoder_out']
+        decoder_out = predictions['decoder_out']
+        original_losses = predictions['losses']
+
+        feats, feat_lens = batch['feat']
+
+        # compute losses
+        losses = {}
+
+        # compute mean loss using lens
+        for key in original_losses:
+            losses[key] = apply_lens_to_loss(original_losses[key], feat_lens)
+
+        # compute recon loss
+        losses['recon_loss'] = compute_masked_loss(self.modules['decoder'].compute_recon_loss,
+                                                   decoder_out, feats,
+                                                   length=feat_lens)
+
+        # compute and save total loss
+        loss = super(SBModel, self).compute_and_save_losses(losses)
+
+        return loss
---
+++ src/modules/h_vae.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from modules.fc_block import FCBlock
+from modules.vanilla_vae import VanillaVAE
+from modules.gmm_vae import GMMVAE
+
+from utils.data_utils import apply_weight
+
+
+class HierarchicalVAE(nn.Module):
+    def __init__(self, fc_sizes, latent_size, num_components):
+        super(HierarchicalVAE, self).__init__()
+
+        # VAE for correct pronunciation
+        self.vanilla_vae = VanillaVAE(fc_sizes, latent_size)
+
+        # VAE for mispronunciation
+        self.gmm_vae = GMMVAE(fc_sizes, latent_size, num_components)
+
+    def forward(
+            self,
+            feats,  # (B, T, C)
+            pi,  # (B, T, 2)
+    ):
+        vae_in = feats  # (B, T, C)
+
+        # vanilla VAE
+        vanilla_out = self.vanilla_vae(vae_in)
+        vanilla_mean = vanilla_out['mean']  # (B, T, C)
+        vanilla_log_var = vanilla_out['log_var']  # (B, T, C)
+        vanilla_h = vanilla_out['sampled_h']  # (B, T, C)
+        vanilla_loss = self.vanilla_vae.compute_kld_loss(vanilla_mean, vanilla_log_var)  # (B, T, C)
+
+        # GMM VAE
+        gmm_out = self.gmm_vae(vae_in)
+        gmm_mean = gmm_out['mean']  # (B, T, N * C)
+        gmm_log_var = gmm_out['log_var']  # (B, T, N * C)
+        gmm_h = gmm_out['sampled_h']  # (B, T, N * C)
+        gmm_loss = self.gmm_vae.compute_kld_loss(gmm_out)  # (B, T, N * C)
+
+        # apply GMM VAE weights
+        gmm_weight = gmm_out['gmm_weight']  # (B, T, N)
+        gmm_mean = apply_weight(gmm_mean, gmm_weight)  # (B, T, C)
+        gmm_log_var = apply_weight(gmm_log_var, gmm_weight)  # (B, T, C)
+        gmm_h = apply_weight(gmm_h, gmm_weight)  # (B, T, C)
+        gmm_loss = apply_weight(gmm_loss, gmm_weight)  # (B, T, C)
+
+        # concatenate vanilla and GMM VAE results
+        mean = torch.stack([vanilla_mean, gmm_mean], dim=2)  # (B, T, 2, C)
+        log_var = torch.stack([vanilla_log_var, gmm_log_var], dim=2)  # (B, T, 2, C)
+        h = torch.stack([vanilla_h, gmm_h], dim=2)  # (B, T, 2, C)
+        kld_loss = torch.stack([vanilla_loss, gmm_loss], dim=2)  # (B, T, C)
+
+        # compute and apply pi
+        mean = apply_weight(mean, pi)  # (B, T, C)
+        log_var = apply_weight(log_var, pi)  # (B, T, C)
+        h = apply_weight(h, pi)  # (B, T, C)
+        kld_loss = apply_weight(kld_loss, pi)  # (B, T, C)
+
+        losses = {
+            'vae_kld_loss': kld_loss  # (B, T, C)
+        }
+
+        return {
+            'gmm_weight': gmm_weight,  # (B, T, N)
+            'mean': mean,  # (B, T, C)
+            'log_var': log_var,  # (B, T, C)
+            'sampled_h': h,  # (B, T, C)
+            'losses': losses
+        }
