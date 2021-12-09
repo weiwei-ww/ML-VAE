189ad8392df8c15e26f7922cfb35806dcb32ffa1
Wei Wei <weiwei-ww@outlook.com>
2021-12-10 03:36:14 +0800

    Update codes for FCBlock

--- src/modules/fc_block.py
+++ src/modules/fc_block.py
@@ -2,12 +2,17 @@ from torch import nn
 
 
 class FCBlock(nn.Module):
-    def __init__(self, fc_sizes, dropout=0.15):
+    def __init__(self, fc_sizes, dropout=0.15, end_activation=False):
         super(FCBlock, self).__init__()
         blocks = nn.ModuleList()
 
-        for i in range(1, len(fc_sizes)):
+        for i in range(1, len(fc_sizes) - 1):
             blocks.append(nn.Linear(fc_sizes[i - 1], fc_sizes[i]))
+            blocks.append(nn.LeakyReLU())
+
+        # the last layer
+        blocks.append(nn.Linear(fc_sizes[-2], fc_sizes[-1]))
+        if end_activation:
             blocks.append(nn.LeakyReLU())
 
         self.blocks = nn.Sequential(*blocks)
