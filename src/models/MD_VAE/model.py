fce38e8a7f959f1051816af34139fe30b840146b
Wei Wei <weiwei-ww@outlook.com>
2021-12-12 03:41:12 +0800

    Add codes for MD_VAE

---
+++ src/models/MD_VAE/model.py
@@ -0,0 +1,123 @@
+import logging
+
+from torch.distributions import Categorical
+import torch.nn
+
+import speechbrain as sb
+from speechbrain.nnet.losses import compute_masked_loss
+from speechbrain.utils.data_utils import undo_padding
+
+from models.md_model import MDModel
+from utils.metric_stats.md_metric_stats import MDMetricStats
+from utils.data_utils import apply_lens_to_loss, undo_padding_tensor
+from utils.decode_utils import decode_plvl_md_lbl_seqs_full as decode_plvl_md_lbl_seqs
+
+logger = logging.getLogger(__name__)
+
+
+class SBModel(MDModel):
+    def on_stage_start(self, stage, epoch=None):
+        super(SBModel, self).on_stage_start(stage, epoch)
+
+        self.stats_loggers['plvl_md_stats'] = MDMetricStats()
+
+
+    def compute_forward(self, batch, stage):
+        batch = batch.to(self.device)
+        feats, feat_lens = batch['feat']
+
+        # initialize predictions
+        predictions = {'losses': {}}
+
+        # feature normalization
+        current_epoch = self.hparams.epoch_counter.current
+        feats = self.hparams.normalizer(feats, feat_lens, epoch=current_epoch)
+
+        # phoneme recognizer
+        plvl_cnnl_phn_seqs, plvl_cnnl_phn_seq_lens = batch['gt_cnncl_seq']
+        fa_boundary_seqs = batch['fa_boundary_seq'][0]
+        phn_recog_out_dict = self.modules['phoneme_recognizer'](
+            feats, feat_lens, plvl_cnnl_phn_seqs, plvl_cnnl_phn_seq_lens, fa_boundary_seqs
+        )
+        predictions['phn_recog_out'] = phn_recog_out_dict['out'].detach().cpu()
+        predictions['losses'].update(phn_recog_out_dict['losses'])
+
+        # FC for phoneme recognizer output
+        phn_recog_fc_out = self.modules['phn_recog_fc'](phn_recog_out_dict['out'])
+
+        # boundary detector
+        fa_boundary_seqs = batch['fa_boundary_seq'][0]
+        b_detector_out = self.modules['boundary_detector'](feats, feat_lens, fa_boundary_seqs)
+        predictions['boundary_v'] = b_detector_out['boundary_v'].detach().cpu()
+        predictions['losses'].update(b_detector_out['losses'])
+
+        # feat FC
+        feat_fc_out = self.modules['feat_fc'](feats)
+
+        # concatenation
+        rnn_in = torch.cat([feat_fc_out, phn_recog_fc_out], dim=-1)
+        rnn_in = self.modules['concat_fc'](rnn_in)
+
+        # RNN
+        rnn_out = self.modules['rnn'](rnn_in)[0]  # (B, T, C)
+
+        # compute Pi
+        pi_logits = self.modules['pi_fc'](rnn_out)  # (B, T, 2)
+        predictions['pi_logits'] = pi_logits.detach().cpu()
+
+        dist = Categorical(logits=pi_logits)
+        if self.modules.training:
+            sampled_pi = dist.sample().float()  # (B, T)
+        else:
+            sampled_pi = torch.argmax(pi_logits, dim=-1).float()  # (B, T)
+        sampled_pi = torch.stack([1 - sampled_pi, sampled_pi], dim=2)  # (B, T, 2)
+        predictions['sampled_pi'] = sampled_pi.detach().cpu()
+
+        # VAE encoder and decoder
+        encoder_out_dict = self.modules['encoder'](rnn_out, sampled_pi)
+        predictions['losses'].update(encoder_out_dict['losses'])
+
+        sampled_h = encoder_out_dict['sampled_h']  # (B, T, C)
+        decoder_out_dict = self.modules['decoder'](sampled_h, feats)
+        predictions['losses'].update(decoder_out_dict['losses'])
+
+        return predictions
+
+    def compute_objectives(self, predictions, batch, stage):
+        # get model outputs
+        original_losses = predictions['losses']
+
+        feats, feat_lens = batch['feat']
+
+        # compute losses
+        losses = {}
+
+        # compute mean loss using lens
+        for key in original_losses:
+            losses[key] = apply_lens_to_loss(original_losses[key], feat_lens)
+
+        # compute and save total loss
+        loss = self.compute_and_save_losses(losses)
+
+        # decoding
+        plvl_cnnl_seqs, plvl_cnnl_seq_lens = batch['gt_cnncl_seq']
+
+        if stage == sb.Stage.VALID or stage == sb.Stage.TEST:
+            weight = getattr(self.hparams, 'dec_weight', 1.0)
+            decoded_boundary_seqs, pred_flvl_md_lbl_seqs, pred_plvl_md_lvl_seqs = decode_plvl_md_lbl_seqs(
+                predictions,
+                utt_ids=batch['id'],
+                feat_lens=feat_lens,
+                plvl_cnnl_seqs=plvl_cnnl_seqs,
+                plvl_cnnl_seq_lens=plvl_cnnl_seq_lens,
+                prior=batch['prior'][0][0],
+                weight=weight
+            )
+            gt_md_lbl_seqs = undo_padding(*batch['plvl_gt_md_lbl_seq'])
+            self.stats_loggers['plvl_md_stats'].append(
+                ids=batch['id'],
+                pred_md_lbl_seqs=pred_plvl_md_lvl_seqs,
+                gt_md_lbl_seqs=gt_md_lbl_seqs,
+            )
+
+        return loss
