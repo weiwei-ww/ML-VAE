d2a1dfadc8cb02961fd3fa315d436ad7c8695067
Wei Wei <weiwei-ww@outlook.com>
2021-12-21 07:30:18 +0800

    Update codes for w2v_MD_VAE

--- src/models/w2v_MD_VAE/model.py
+++ src/models/w2v_MD_VAE/model.py
@@ -26,32 +26,30 @@ class SBModel(MD_VAE):
             raise ValueError('target is not defined')
 
         batch = batch.to(self.device)
-        # feats, feat_lens = batch['feat']
+        feats, feat_lens = batch['feat']
 
         wavs, wav_lens = batch['wav']
-        feats = self.modules['wav2vec2'](wavs)  # (B, T, 1024)
-        feat_lens = batch['feat'][1]
+        w2v_feats = self.modules['wav2vec2'](wavs)  # (B, T, 1024)
 
         # handle inconsistent lengths
-        ori_feats = batch['feat'][0]
-        shape_diff = feats.shape[1] - ori_feats.shape[1]
+        shape_diff = w2v_feats.shape[1] - feats.shape[1]
         assert -2 <= shape_diff <= 0, f'shape_diff = {shape_diff}'
         if shape_diff < 0:
-            zeros = torch.zeros(feats.shape[0], abs(shape_diff), feats.shape[2]).float().to(feats.device)
-            feats = torch.concat([feats, zeros], dim=1)
-        assert feats.shape[1] == ori_feats.shape[1]
+            zeros = torch.zeros(w2v_feats.shape[0], abs(shape_diff), w2v_feats.shape[2]).float().to(feats.device)
+            w2v_feats = torch.concat([w2v_feats, zeros], dim=1)
+        assert w2v_feats.shape[1] == feats.shape[1]
 
         # initialize predictions
-        fake_loss = torch.zeros_like(feats)
-        fake_loss.requires_grad = True
-        predictions = {'losses': {'loss': fake_loss}}
+        predictions = {'losses': {}}
 
         # phoneme recognizer
         if self.target in [Target.PHN_RECOG, Target.VAE, Target.TEST]:
+            phn_recog_in_fc_out = self.modules['phn_recog_in_fc'](w2v_feats)
+
             plvl_cnnl_phn_seqs, plvl_cnnl_phn_seq_lens = batch['gt_cnncl_seq']
             fa_boundary_seqs = batch['fa_boundary_seq'][0]
             phn_recog_out_dict = self.modules['phoneme_recognizer'](
-                feats, feat_lens, plvl_cnnl_phn_seqs, plvl_cnnl_phn_seq_lens, fa_boundary_seqs
+                phn_recog_in_fc_out, feat_lens, plvl_cnnl_phn_seqs, plvl_cnnl_phn_seq_lens, fa_boundary_seqs
             )
             predictions['phn_recog_out'] = phn_recog_out_dict['out']
 
@@ -61,14 +59,12 @@ class SBModel(MD_VAE):
                     losses[key] = losses[key].detach()
             predictions['losses'].update(losses)
 
-            loss_keys = predictions['losses'].keys()
-            logger.info(f'losses for phn_recog: {loss_keys}')
-
-
         # boundary detector
         if self.target in [Target.B_DETECTOR, Target.VAE, Target.TEST]:
+            b_detector_in_fc_out = self.modules['b_detector_in_fc'](w2v_feats)
+
             fa_boundary_seqs = batch['fa_boundary_seq'][0]
-            b_detector_out = self.modules['boundary_detector'](feats, feat_lens, fa_boundary_seqs)
+            b_detector_out = self.modules['boundary_detector'](b_detector_in_fc_out, feat_lens, fa_boundary_seqs)
             predictions['boundary_v'] = b_detector_out['boundary_v']
 
             losses = b_detector_out['losses']
@@ -80,13 +76,13 @@ class SBModel(MD_VAE):
         # VAE
         if self.target in [Target.VAE, Target.TEST]:
             # feat FC
-            feat_fc_out = self.modules['feat_fc'](feats)
+            w2v_feat_fc_out = self.modules['w2v_feat_fc'](w2v_feats)
 
             # FC for phoneme recognizer output
-            phn_recog_fc_out = self.modules['phn_recog_fc'](phn_recog_out_dict['out'].detach())
+            phn_recog_out_fc_out = self.modules['phn_recog_out_fc'](phn_recog_out_dict['out'].detach())
 
             # concatenation
-            rnn_in = torch.cat([feat_fc_out, phn_recog_fc_out], dim=-1)
+            rnn_in = torch.cat([w2v_feat_fc_out, phn_recog_out_fc_out], dim=-1)
             rnn_in = self.modules['concat_fc'](rnn_in)
 
             # RNN
