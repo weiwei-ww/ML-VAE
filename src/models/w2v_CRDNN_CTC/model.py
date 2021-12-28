e981506e0f827eecbc690b04080a61e619a6e2ab
Wei Wei <weiwei-ww@outlook.com>
2021-12-28 23:55:51 +0800

    Remove useless imports

--- src/models/w2v_CRDNN_CTC/model.py
+++ src/models/w2v_CRDNN_CTC/model.py
@@ -1,13 +1,4 @@
 import logging
-from pathlib import Path
-
-import speechbrain as sb
-from speechbrain.utils.data_utils import undo_padding
-from speechbrain.utils.metric_stats import ErrorRateStats
-from speechbrain.nnet.losses import ctc_loss
-
-import utils.alignment
-from utils.metric_stats.md_metric_stats import MDMetricStats
 import models.CRDNN_CTC.model as CRDNN_CTC
 
 logger = logging.getLogger(__name__)
