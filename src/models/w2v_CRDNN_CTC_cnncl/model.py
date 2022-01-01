1bc791997f4cf8a43c54ccbeb4b0ce09679309aa
Wei Wei <weiwei-ww@outlook.com>
2022-01-02 07:37:32 +0800

    Add codes for saving model outputs

--- src/models/w2v_CRDNN_CTC_cnncl/model.py
+++ src/models/w2v_CRDNN_CTC_cnncl/model.py
@@ -1,25 +1,21 @@
 import logging
+import warnings
 from pathlib import Path
 
 import torch
 import speechbrain as sb
-from speechbrain.utils.data_utils import undo_padding
-from speechbrain.utils.metric_stats import ErrorRateStats
-from speechbrain.nnet.losses import ctc_loss
 
-import utils.alignment
-from utils.metric_stats.md_metric_stats import MDMetricStats
-from utils.data_utils import undo_padding_tensor
+from utils.data_utils import undo_padding_tensor, resample_tensor
 import models.CRDNN_CTC_cnncl.model as CRDNN_CTC_cnncl
 
 logger = logging.getLogger(__name__)
 
 
 class SBModel(CRDNN_CTC_cnncl.SBModel):
-    # def on_stage_start(self, stage, epoch=None):
-    #     super(SBModel, self).on_stage_start(stage, epoch)
-    #     self.saved_pouts = {}  # save pout to write to file
-
+    def on_stage_start(self, stage, epoch=None):
+        super(SBModel, self).on_stage_start(stage, epoch)
+        if stage == sb.Stage.TEST:
+            self.saved_pouts = {}  # save pout to write to file
 
     def compute_forward(self, batch, stage):
         batch = batch.to(self.device)
@@ -35,26 +31,36 @@ class SBModel(CRDNN_CTC_cnncl.SBModel):
             'pout': pout,
         }
 
-        # feats, feat_lens = batch['feat']
-        # unpadded_pouts = undo_padding_tensor(pout, feat_lens)
-        # unpadded_feats = undo_padding_tensor(feats, feat_lens)
-        # ids = batch['id']
-        # for id, unpadded_pout, feat in zip(ids, unpadded_pouts, unpadded_feats):
-        #     self.saved_pouts[id] = unpadded_pout.detach()
+        if stage == sb.Stage.TEST:
+            feats, feat_lens = batch['feat']
+            unpadded_pouts = undo_padding_tensor(pout, feat_lens)
+            unpadded_feats = undo_padding_tensor(feats, feat_lens)
+            ids = batch['id']
+            for id, unpadded_pout, feat in zip(ids, unpadded_pouts, unpadded_feats):
+                unpadded_pout = unpadded_pout.detach()
+                unpadded_pout = resample_tensor(unpadded_pout, feat, dim=0)
+                self.saved_pouts[id] = unpadded_pout
 
         return predictions
 
-    # def on_stage_end(self, stage, stage_loss, epoch=None):
-    #     super(SBModel, self).on_stage_end(stage, stage_loss, epoch)
+    # def compute_objectives(self, predictions, batch, stage):
+    #     loss = super(SBModel, self).compute_objectives(predictions, batch, stage)
     #
-    #     if stage == sb.Stage.TEST:
-    #         saved_pouts_path = Path(self.hparams.hparams) / 'saved_pouts.pt'
-    #         if saved_pouts_path.exists():
-    #             saved_pouts = torch.load(saved_pouts_path)
-    #         else:
-    #             saved_pouts = {}
-    #         for key in self.saved_pouts:
-    #             if key in saved_pouts:
-    #                 raise ValueError(f'duplicate key {key}')
-    #         saved_pouts.update(self.saved_pouts)
-    #         torch.save(saved_pouts, saved_pouts_path)
+    #     logger.info(f'{self.hparams.epoch_counter.current}, {loss}')
+    #
+    #     return loss
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        super(SBModel, self).on_stage_end(stage, stage_loss, epoch)
+
+        if stage == sb.Stage.TEST:
+            saved_pouts_path = Path(self.hparams.output_dir) / 'saved_phn_recog_outs.pt'
+            if saved_pouts_path.exists():
+                saved_pouts = torch.load(saved_pouts_path)
+            else:
+                saved_pouts = {}
+            for key in self.saved_pouts:
+                if key in saved_pouts:
+                    warnings.warn(f'duplicate key {key}')
+            saved_pouts.update(self.saved_pouts)
+            torch.save(saved_pouts, saved_pouts_path)
