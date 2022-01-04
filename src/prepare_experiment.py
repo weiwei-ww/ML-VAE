560797fbb12bec82506013a0df265125fa717fb7
Wei Wei <weiwei-ww@outlook.com>
2022-01-04 09:34:25 +0800

    Update codes for experiment preparation

--- src/prepare_experiment.py
+++ src/prepare_experiment.py
@@ -33,32 +33,28 @@ def prepare_experiment(args, prepare_exp_dir):
             overrides=[extra_overrides, overrides]
         )
 
+    prepared = {'hparams': hparams}
+
     # json file preparation
     dataset_name = hparams['dataset']
     importlib.import_module(f'datasets.{dataset_name}.prepare').prepare(**hparams['prepare'])
 
-
     # Load parsed dataset
     datasets, label_encoder = prepare_datasets(hparams)
+    prepared['datasets'] = datasets
 
     # initialize model
-    model_class = hparams['model_class']
-    SBModel = importlib.import_module(f'models.{model_class}.model').SBModel
-    model = SBModel(
-        label_encoder=label_encoder,
-        modules=hparams['model']['modules'],
-        # opt_class=hparams['model']['optimizer'],
-        hparams=hparams['model'],
-        run_opts=run_opts,
-        checkpointer=hparams['model']['checkpointer'],
-    )
-
-    prepared = {
-        'hparams': hparams,
-        # 'hparams_file': hparams_file,
-        # 'overrides': overrides,
-        'datasets': datasets,
-        'model': model
-    }
+    if 'model_class' in hparams:
+        model_class = hparams['model_class']
+        SBModel = importlib.import_module(f'models.{model_class}.model').SBModel
+        model = SBModel(
+            label_encoder=label_encoder,
+            modules=hparams['model']['modules'],
+            # opt_class=hparams['model']['optimizer'],
+            hparams=hparams['model'],
+            run_opts=run_opts,
+            checkpointer=hparams['model']['checkpointer'],
+        )
+        prepared['model'] = model
 
     return prepared
