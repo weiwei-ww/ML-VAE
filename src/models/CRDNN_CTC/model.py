a2580308e7013d8e9731a1eb4a7b6b7379d8e47d
Wei Wei <weiwei-ww@outlook.com>
2022-01-04 09:34:00 +0800

    Add more metrics to CTC models

--- src/models/CRDNN_CTC/model.py
+++ src/models/CRDNN_CTC/model.py
@@ -1,12 +1,16 @@
 import logging
 from pathlib import Path
+import numpy as np
+import torch
+from ctc_segmentation import CtcSegmentationParameters, ctc_segmentation, determine_utterance_segments
 
 import speechbrain as sb
 from speechbrain.utils.data_utils import undo_padding
 from speechbrain.utils.metric_stats import ErrorRateStats
 from speechbrain.nnet.losses import ctc_loss
 
-import utils.alignment
+from utils.alignment import batch_align_sequences
+from utils.data_utils import undo_padding_tensor, resample_tensor
 from utils.metric_stats.md_metric_stats import MDMetricStats
 from models.md_model import MDModel
 
@@ -92,14 +96,21 @@ class SBModel(MDModel):
         gt_cnncl_seqs = undo_padding(gt_cnncl_seqs, gt_cnncl_seq_lens)
 
         # align sequences
-        ali_pred_phn_seqs, ali_gt_phn_seqs, ali_gt_cnncl_seqs = \
-            utils.alignment.batch_align_sequences(pred_phns, gt_phn_seqs, gt_cnncl_seqs)
+        ali_gt_phn_seqs, ali_pred_phn_seqs, ali_gt_cnncl_seqs = \
+            batch_align_sequences(gt_phn_seqs, pred_phns, gt_cnncl_seqs)
+
+        # compute CTC segmentation results
+        ctc_segmentation_boundary_seqs = self.compute_ctc_segmentation(batch, pout)
+        gt_boundary_seqs = undo_padding_tensor(*batch['gt_boundary_seq'])
+        gt_boundary_seqs = [seq.cpu() for seq in gt_boundary_seqs]
 
         self.stats_loggers['plvl_md_stats'].append(
             batch['id'],
             pred_phn_seqs=ali_pred_phn_seqs,
             gt_phn_seqs=ali_gt_phn_seqs,
-            gt_cnncl_seqs=ali_gt_cnncl_seqs
+            gt_cnncl_seqs=ali_gt_cnncl_seqs,
+            pred_boundary_seqs=ctc_segmentation_boundary_seqs,
+            gt_boundary_seqs=gt_boundary_seqs
         )
 
         return loss
@@ -110,3 +121,43 @@ class SBModel(MDModel):
         if stage == sb.Stage.TEST:
             output_path = Path(self.hparams.output_dir) / 'test_output' / 'md_result_seqs.txt'
             self.stats_loggers['plvl_md_stats'].write_seqs_to_file(output_path, self.label_encoder)
+
+    def compute_ctc_segmentation(self, batch, pouts):
+        feats, feat_lens = batch['feat']
+        pouts = resample_tensor(pouts, feats, dim=1)
+
+        unpadded_pouts = undo_padding_tensor(pouts, feat_lens)
+        assert len(batch['id']) == len(unpadded_pouts)
+        unpadded_gt_cnncl_seq = undo_padding(*batch['gt_cnncl_seq'])
+
+        config = CtcSegmentationParameters()
+        config.char_list = list(range(pouts.shape[-1]))
+        boundary_seqs = []
+        for b, (utt_id, pout) in enumerate(zip(batch['id'], unpadded_pouts)):
+            pout = pout.cpu().detach().numpy()
+            y = unpadded_gt_cnncl_seq[b]  # shape = (L)
+            new_y = [-1, 0]
+            utt_start_indices = [1]
+            for item in y:
+                new_y.extend([item])
+                utt_start_indices.append(len(new_y) - 1)
+            new_y = np.array(new_y).reshape(-1, 1)
+
+            timings, char_probs, state_list = ctc_segmentation(config, pout, new_y)
+            segments = determine_utterance_segments(
+                config, utt_start_indices, char_probs, timings, utt_start_indices[:-1]
+            )
+
+            boundary_seq = torch.zeros(pout.shape[0])
+            for i, (start, end, _) in enumerate(segments):
+                if i == 0:
+                    start_index = 0
+                else:
+                    start_index = int(np.ceil(start / config.index_duration))
+                end_index = int(np.ceil(end / config.index_duration))
+                assert start_index < end_index
+                assert boundary_seq[start_index] == 0
+                boundary_seq[start_index] = 1
+            boundary_seqs.append(boundary_seq)
+
+        return boundary_seqs
