dfe43dab991d93706f5ecf54e788f9c1b0c6f93f
Wei Wei <weiwei-ww@outlook.com>
2022-01-28 00:44:32 +0800

    Add codes for parallel decoding

--- src/utils/decode_utils.py
+++ src/utils/decode_utils.py
@@ -1,3 +1,5 @@
+import time
+from joblib import Parallel, delayed
 import numpy as np
 
 import torch
@@ -186,7 +188,7 @@ def decode_phoneme_mdd_conditional(
     return phoneme_mdd_ret
 
 
-def decode_plvl_md_lbl_seqs_full(
+def decode_plvl_md_lbl_seqs_full_non_par(
         predictions,
         utt_ids,  # len = B
         feat_lens,  # shape = (B)
@@ -196,7 +198,7 @@ def decode_plvl_md_lbl_seqs_full(
         weight=1.0
 ):
     '''
-    Decode the boundaries and the beta sequence simultaneously.
+    Decode the boundaries and the beta sequence simultaneously. Non-parallel version.
 
     Parameters
     ----------
@@ -248,7 +250,8 @@ def decode_plvl_md_lbl_seqs_full(
     decoded_boundary_seqs = []
     flvl_md_lbl_seqs = []
     plvl_md_lbl_seqs = []
-    
+
+    start_time = time.time()
     for i, utt_id in enumerate(utt_ids):
         T_i = feat_lens[i]
         L_i = plvl_cnnl_seq_lens[i]
@@ -359,6 +362,8 @@ def decode_plvl_md_lbl_seqs_full(
         # save phoneme mdd results
         flvl_md_lbl_seqs.append(frame_mdd_ret_i)
         plvl_md_lbl_seqs.append(phoneme_mdd_ret_i)
+    end_time = time.time()
+    print(f'Time elapsed for decoding: {round(end_time - start_time, 2)} seconds')
 
     # print(f'{np.sum([np.sum(l) for l in flvl_md_lbl_seqs])}/{np.sum([len(l) for l in flvl_md_lbl_seqs])}')
     # print(f'{np.sum([np.sum(l) for l in plvl_md_lbl_seqs])}/{np.sum([len(l) for l in plvl_md_lbl_seqs])}')
@@ -366,4 +371,195 @@ def decode_plvl_md_lbl_seqs_full(
     return decoded_boundary_seqs, flvl_md_lbl_seqs, plvl_md_lbl_seqs
 
 
+def decode_plvl_md_lbl_seqs_full(
+        predictions,
+        utt_ids,  # len = B
+        feat_lens,  # shape = (B)
+        plvl_cnnl_seqs,  # shape = (B, L, N)
+        plvl_cnnl_seq_lens,  # shape = (B)
+        prior,  # shape = (N)
+        weight=1.0
+):
+    '''
+    Decode the boundaries and the beta sequence simultaneously. The parallel version.
 
+    Parameters
+    ----------
+    predictions : dict
+        Model outputs.
+    utt_ids ： list
+        A list of IDs.
+    feat_lens : torch.Tensor
+        Frame lengths of each sample in the batch (relative length).
+    plvl_cnnl_seqs : torch.Tensor
+        Phoneme level canonical phoneme sequences.
+    plvl_cnnl_seq_lens : torch.Tensor
+        Phoneme lengths of each sample in the batch (relative length).
+    prior : torch.Tensor
+        Prior distribution of the phonemes.
+    weight : float
+        Weight for decoding.
+
+    Returns
+    -------
+
+    '''
+    # get the absolute lengths
+    feat_lens = torch.round(feat_lens * predictions['phn_recog_out'].shape[1]).int()
+    plvl_cnnl_seq_lens = torch.round(plvl_cnnl_seq_lens * plvl_cnnl_seqs.shape[1]).int()
+
+    # pre-computation
+    phn_recog_out = torch.sigmoid(predictions['phn_recog_out'])  # shape = (B, T, N)
+    p_yx = torch.stack([phn_recog_out, 1 - phn_recog_out], dim=3)  # shape = (B, T, N, 2)
+    log_p_yx = log(p_yx)  # shape = (B, T, N, 2)
+
+    p_y = torch.stack([prior, 1 - prior], dim=1)  # shape = (N, 2)
+    log_p_y = log(p_y)  # shape = (N, 2)
+
+    y = plvl_cnnl_seqs  # shape = (B, L)
+
+    boundary_v = predictions['boundary_v']  # shape = (B, T)
+    p_b = torch.stack([boundary_v, 1 - boundary_v], dim=2)  # shape = (B, T, 2)
+    log_p_b = log(p_b)  # shape = (B, T, 2)
+
+    pi_logits = predictions['pi_logits']  # shape = (B, T, 2)
+    p_pi = torch.softmax(pi_logits, dim=-1)  # shape = (B, T, 2)
+    log_p_pi = log(p_pi)  # shape = (B, T, 2)
+
+    decoded_boundary_seqs = []
+    flvl_md_lbl_seqs = []
+    plvl_md_lbl_seqs = []
+
+    # start_time = time.time()
+
+    # convert to np.array
+    feat_lens = feat_lens.cpu().numpy()
+    plvl_cnnl_seq_lens = plvl_cnnl_seq_lens.cpu().numpy()
+    y = y.cpu().numpy()
+
+    def decode_one_utt(i):
+        T_i = feat_lens[i]
+        L_i = plvl_cnnl_seq_lens[i]
+
+        log_p_yx_i = log_p_yx[i, :T_i, :]  # shape = (T_i, N, 2)
+        y_i = y[i, :L_i]  # shape = (L_i)
+        log_p_b_i = log_p_b[i, :T_i, :]  # shape = (T_i, 2)
+        log_p_pi_i = log_p_pi[i, :T_i, :]  # shape = (T_i, 2)
+
+        dp_value = np.zeros((L_i, T_i, 2), dtype=float)
+        dp_value.fill(-np.inf)
+
+        dp_path = np.zeros((L_i, T_i, 2), dtype=int)
+        dp_path.fill(-1)
+
+        dp_value[0, 0, 0] = weight * log_p_pi_i[0, 0] + log_p_yx_i[0, y_i[0], 0] - log_p_y[y_i[0], 0]
+        dp_value[0, 0, 1] = weight * log_p_pi_i[0, 1] + log_p_yx_i[0, y_i[0], 1] - log_p_y[y_i[0], 1]
+
+        # decode with dynamic programming
+        for l in range(L_i):
+            for t in range(1, T_i):
+                if l == 0:
+                    # correct pronunciation
+                    dp_value[l, t, 0] = \
+                        dp_value[l, t - 1, 0] + log_p_b_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 0] - log_p_y[y_i[l], 0]
+                    dp_path[l, t, 0] = 0
+                    # mispronunciation
+                    dp_value[l, t, 1] = \
+                        dp_value[l, t - 1, 1] + log_p_b_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 1] - log_p_y[y_i[l], 1]
+                    dp_path[l, t, 1] = 0
+                else:
+                    # correct pronunciation
+                    hold_value = \
+                        dp_value[l, t - 1, 0] + log_p_b_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 0] - log_p_y[y_i[l], 0]  # b = 0
+                    from_correct_value = \
+                        dp_value[l - 1, t - 1, 0] + log_p_b_i[t, 1] + weight * log_p_pi_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 0] - log_p_y[y_i[l], 0]  # b = 1
+                    from_incorrect_value = \
+                        dp_value[l - 1, t - 1, 1] + log_p_b_i[t, 1] + weight * log_p_pi_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 0] - log_p_y[y_i[l], 0]  # b = 1
+
+                    value_list = [hold_value, from_correct_value, from_incorrect_value]
+                    dp_value[l, t, 0] = np.max(value_list)
+                    dp_path[l, t, 0] = np.argmax(value_list)
+
+                    # mispronunciation
+                    hold_value = \
+                        dp_value[l, t - 1, 1] + log_p_b_i[t, 0] + \
+                        log_p_yx_i[t, y_i[l], 1] - log_p_y[y_i[l], 1]  # b = 0
+                    from_correct_value = \
+                        dp_value[l - 1, t - 1, 0] + log_p_b_i[t, 1] + weight * log_p_pi_i[t, 1] + \
+                        log_p_yx_i[t, y_i[l], 1] - log_p_y[y_i[l], 1]  # b = 1
+                    from_incorrect_value = \
+                        dp_value[l - 1, t - 1, 1] + log_p_b_i[t, 1] + weight * log_p_pi_i[t, 1] + \
+                        log_p_yx_i[t, y_i[l], 1] - log_p_y[y_i[l], 1]  # b = 1
+
+                    value_list = [hold_value, from_correct_value, from_incorrect_value]
+                    dp_value[l, t, 1] = np.max(value_list)
+                    dp_path[l, t, 1] = np.argmax(value_list)
+
+        # backtracking
+        l, t = L_i - 1, T_i - 1
+        boundary_index_i = []
+        frame_mdd_ret_i = []
+        phoneme_mdd_ret_i = []
+
+        if dp_value[l, t, 0] > dp_value[l, t, 1]:
+            frame_mdd_ret_i.append(0)
+            phoneme_mdd_ret_i.append(0)
+            beta = 0
+        else:
+            phoneme_mdd_ret_i.append(1)
+            frame_mdd_ret_i.append(1)
+            beta = 1
+        while t > 0:
+            if dp_path[l, t, beta] == 1:
+                l -= 1
+                boundary_index_i.append(int(t))
+                frame_mdd_ret_i.append(0)
+                phoneme_mdd_ret_i.append(0)
+                beta = 0
+            elif dp_path[l, t, beta] == 2:
+                l -= 1
+                boundary_index_i.append(int(t))
+                frame_mdd_ret_i.append(1)
+                phoneme_mdd_ret_i.append(1)
+                beta = 1
+            else:
+                frame_mdd_ret_i.append(frame_mdd_ret_i[-1])
+            t -= 1
+        boundary_index_i.append(int(t))
+        assert l == t == 0, 'l = {}, t = {}'.format(l, t)
+        boundary_index_i.reverse()
+        frame_mdd_ret_i.reverse()
+        phoneme_mdd_ret_i.reverse()
+
+        # convert boundary indices into binary boundaries
+        decoded_boundary_seq = np.zeros(T_i, dtype=int)
+        decoded_boundary_seq[boundary_index_i] = 1
+        assert decoded_boundary_seq.sum() == L_i
+
+        return decoded_boundary_seq, frame_mdd_ret_i, phoneme_mdd_ret_i
+
+    B = len(utt_ids)
+    decoded_result_list = Parallel(n_jobs=B)(delayed(decode_one_utt)(i) for i in range(B))
+    for decoded_boundary_seq, frame_mdd_ret_i, phoneme_mdd_ret_i in decoded_result_list:
+        decoded_boundary_seqs.append(decoded_boundary_seq)
+        flvl_md_lbl_seqs.append(frame_mdd_ret_i)
+        plvl_md_lbl_seqs.append(phoneme_mdd_ret_i)
+
+    # for i in range(len(utt_ids)):
+    #     decoded_boundary_seq, frame_mdd_ret_i, phoneme_mdd_ret_i = decode_one_utt(i)
+    #     decoded_boundary_seqs.append(decoded_boundary_seq)
+    #     flvl_md_lbl_seqs.append(frame_mdd_ret_i)
+    #     plvl_md_lbl_seqs.append(phoneme_mdd_ret_i)
+
+    # end_time = time.time()
+    # print(f'Time elapsed for decoding: {round(end_time - start_time, 2)} seconds')
+
+    # print(f'{np.sum([np.sum(l) for l in flvl_md_lbl_seqs])}/{np.sum([len(l) for l in flvl_md_lbl_seqs])}')
+    # print(f'{np.sum([np.sum(l) for l in plvl_md_lbl_seqs])}/{np.sum([len(l) for l in plvl_md_lbl_seqs])}')
+
+    return decoded_boundary_seqs, flvl_md_lbl_seqs, plvl_md_lbl_seqs
